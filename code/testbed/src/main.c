f469e26d1211c99536d2ba79c839db8b6b64c72b
essalguero <essalguero@yahoo.es>
2022-07-06 01:19:17 +0200

    Added logging and assertions

--- code/testbed/src/main.c
+++ code/testbed/src/main.c
@@ -1,6 +1,15 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("A test message: %f", 3.14f);
+    KERROR("A test message: %f", 3.14f);
+    KWARN("A test message: %f", 3.14f);
+    KINFO("A test message: %f", 3.14f);
+    KDEBUG("A test message: %f", 3.14f);
+    KTRACE("A test message: %f", 3.14f);
+
+    KASSERT_MSG(1 == 0, "Error in comparison");
+    
     return 0;
 }
